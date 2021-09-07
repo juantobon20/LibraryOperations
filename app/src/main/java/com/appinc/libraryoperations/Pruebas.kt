afcc4815d7c3c41a2ed6d5adbea0a672c5d08db1
Juan Camilo Tobon <camilotq@abakosoluciones.com>
2021-09-06 20:51:54 -0500

    adas

---
+++ app/src/main/java/com/appinc/libraryoperations/Pruebas.kt
@@ -0,0 +1,11 @@
+package com.appinc.libraryoperations
+
+import com.appinc.operationsmodule.OperationsJT
+
+class Pruebas {
+
+    fun operations(){
+        val op = OperationsJT()
+        val sum = op.sumar(1,1)
+    }
+}
